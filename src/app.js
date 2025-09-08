ddc4db10d2223e9754e130d5c1c9150d8a884863
camilajuliao <juliaocamila13@gmail.com>
2025-09-07 22:13:46 -0400

    fixing routes on app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 //users
-app.get('api/v1/user/get-all-users', validateJwt, getAllUsers);
+app.get('/api/v1/user/get-all-users', validateJwt, getAllUsers);
 
 //cupos
-app.get('api/v1/users/:id/cupos', validateJwt, validateUserExist, getCuposByMovilNumber);
+app.get('/api/v1/users/:id/cupos', validateJwt, validateUserExist, getCuposByMovilNumber);
